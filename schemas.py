7d90799f9ffed2c346018b008606e2439d0601c7
Админ <lenzik_1991@mail.ru>
2022-06-27 19:12:57 +0500

    Initial commit

---
+++ schemas.py
@@ -0,0 +1,14 @@
+from marshmallow import Schema, fields
+
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Str()
+    director_id = fields.Str()
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
